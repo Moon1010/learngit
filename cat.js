f9f61472c0b77507944d04af855a95054c64f90d
Nguyet <nguyetdoan1010@gmail.com>
2020-03-08 16:36:39 +0700

    complete

--- cat.js
+++ cat.js
@@ -1,10 +1,6 @@
 function Cat() {
 	console.log("This is a nice cat!");
-<<<<<<< HEAD
+
 }
 
 module.exports = Cat;
-//check
-=======
-}
->>>>>>> parent of 8966458... Fix: missing export
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var Dog = require('./dog');
+var Cat = require('./cat');
 
 var dog = new Dog('Puppy');
 dog.sayHi();
+
+console.log('I love animals');
