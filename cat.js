e0dc0105cf0beb19b84f085c57fced42ce0957da
Nguyet <nguyetdoan1010@gmail.com>
2020-03-08 15:19:00 +0700

    add context for cat.js

--- cat.js
+++ cat.js
@@ -1,10 +1,8 @@
 function Cat() {
 	console.log("This is a nice cat!");
-<<<<<<< HEAD
+
 }
 
 module.exports = Cat;
-//check
-=======
-}
->>>>>>> parent of 8966458... Fix: missing export
+
+//check git revert
